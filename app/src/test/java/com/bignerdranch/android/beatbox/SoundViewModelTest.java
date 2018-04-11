647136381bdb56c061e99d972221bff2c38c3010
MichaelG <micgon7@gmail.com>
2018-04-11 09:43:35 -0400

    added additional test onSeekMove and GetPlayRate methods.

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.beatbox;
 
+import android.widget.SeekBar;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyFloat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -15,7 +18,11 @@ import static org.mockito.Mockito.verify;
 public class SoundViewModelTest {
     private BeatBox mBeatBox;
     private Sound mSound;
+    private SeekBar mSeekBar;
     private SoundViewModel mSubject; // Makes clear the subject of the test.
+
+    // TODO Ask Matt about Spy().
+
     @Before
     public void setUp() throws Exception {
         // automatically create a mocked out version of BeatBox (no method definitions).
@@ -23,7 +30,9 @@ public class SoundViewModelTest {
 
         mSound = new Sound("assetPath");
         mSubject = new SoundViewModel(mBeatBox);
+        mSeekBar = mock(SeekBar.class);
         mSubject.setSound(mSound);
+
     }
 
     @Test
@@ -40,8 +49,19 @@ public class SoundViewModelTest {
     }
 
     @Test
-    public void callsOnProgressChange() {
+    public void callsOnSeekMove() {
+        mSubject.onSeekBarMove(mSeekBar);
 
+        verify(mBeatBox).setPlayRate(anyFloat());
+
+
+    }
+
+    @Test
+    public void callsGetPlayRate() {
+        mSubject.getPlayRateText();
+
+        verify(mBeatBox).getPlayRate();
     }
 
 }
